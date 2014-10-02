37209b994b0ef9e35f40927483ba1aa871ed68e3
Ben <ben35johnson@gmail.com>
2014-10-02 17:13:48 -0500

    USOpenFinal

--- SWFAA_WDT.ino
+++ SWFAA_WDT.ino
@@ -7,15 +7,22 @@ const int _Pat = 0; //interrupt #
 const int _PatFreq = 1000;
 const int _biteDuration = 200;
 const int _startupDelay = 3000;
+const int _patResetCount = 3;
 
 uint32_t bite_time;
 bool wdtEnabled;
+int currentPats;
 
 void setup() 
 {
 	//Set bite as HIGH
 	digitalWrite(_Bite, HIGH);
 	pinMode(_Bite, OUTPUT); 
+
+	currentPats = 0;
+
+	Serial.begin(115200);
+  	while (!Serial); 
 
 	//set enabled to false, wait until first pat to enable
 	wdtEnabled = false;
@@ -28,23 +35,41 @@ void setup()
 
 	//Attach interrupt to call pat function on change
 	attachInterrupt(_Pat, pat, CHANGE);
+	Serial.println("started");
 }
 
 void loop() 
 {
 	if (wdtEnabled && millis() >= bite_time)
 	{
-		detachInterrupt(_Pat, pat);
+		Serial.println("reset");
+		detachInterrupt(_Pat);
 		//Reset
 		digitalWrite(_Bite, LOW);
 		delay(_biteDuration);
 		digitalWrite(_Bite, HIGH);
+
+		Serial.println("disabled");
 		wdtEnabled = false;
+		currentPats = 0;
+		attachInterrupt(_Pat, pat, CHANGE);
 	}
 }
 
 void pat()
 {
-	wdtEnabled = true;
+	Serial.println("pat");
+	if(!wdtEnabled)
+	{
+		if(_patResetCount <= currentPats)
+		{
+			Serial.println("enabled");
+			wdtEnabled = true;
+		}
+		else
+		{
+			currentPats++;
+		}
+	}
 	bite_time = millis() + _PatFreq;
 }
